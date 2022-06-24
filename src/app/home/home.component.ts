bdf48d4136e572ce15b28847e35d5a933cd894c4
= <=>
2022-06-24 15:05:49 +0700

    DataBindingValue

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  //khai báo biến
+  public name = 'Tan Dang';
   constructor() { }
 
   ngOnInit(): void {
