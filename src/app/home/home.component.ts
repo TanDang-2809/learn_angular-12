d76ca0175a4ed085a3a8b861bcb2877c25a91842
= <=>
2022-06-25 08:58:04 +0700

    ngIf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   //khai báo biến
   public name = 'Tan Dang';
-
+  public age = 15;
   constructor() { 
    
   }
