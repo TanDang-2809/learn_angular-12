b0f2af22de99d0ee8499de7b331f95c6650b4673
= <=>
2022-06-26 14:10:53 +0700

    ngForApDung

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,10 +10,18 @@ export class HomeComponent implements OnInit {
   public name = 'Tan Dang';
   //2 phần tử trong mảng
   public traiCay = ['Cam', 'Táo', 'Xoài', 'Ổi'];
+  //Mảng gồm 2 Object
+  public traiCay2 = [
+    {ten : 'Táo', gia : 12, haGia : true},
+    {ten : 'Nho', gia : -13, haGia : false},
+    {ten : 'Cam', gia : 22.22, haGia : true},
+    {ten : 'Sầu riêng', gia : 33.33, haGia : false}
+  ];
   constructor() {}
 
   public ngOnInit(): void {
-    console.log('Các loại trái cây : ', this.traiCay);
+    console.log('Các loại trái cây =  ', this.traiCay);
+    console.log('Chuỗi trái cây = ', this.traiCay2);
   }
 
 }
